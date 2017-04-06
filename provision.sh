9852305429a46e70c770182e99df1874e4af609f
Roelof Roos <github@roelof.io>
2017-04-06 13:33:13 +0200

    Some bug fixes

--- provision.sh
+++ provision.sh
@@ -1,13 +1,31 @@
 #!/usr/bin/env bash
 
+if [ -z "$1" ]
+then
+    echo Please specify admin username as first argument
+    exit 255
+fi
+
+if [ -z "$2" ]
+then
+    echo Please specify admin e-mail as second argument
+    exit 255
+fi
+
+# Get verbose
 set -xe
+
+# Download WordPress
+wp core download
 
 # Install wordpress
 if ! wp core is-installed
 then
     wp core install \
         --url="https://spirithunt.win" \
-        --title="Spirit Hunt"
+        --title="Spirit Hunt" \
+        --admin_user="${1}" \
+        --admin_email="${2}"
 fi
 
 # Options
